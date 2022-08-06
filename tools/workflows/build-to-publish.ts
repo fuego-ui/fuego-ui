585bd34701166560264e27a951bcfdac7bb447d7
Sammy Mohamed <sammymohamed1223@gmail.com>
2022-08-06 19:09:48 -0400

    Fuego Lib Publish Script

--- tools/workflows/build-to-publish.ts
+++ tools/workflows/build-to-publish.ts
@@ -1,6 +1,7 @@
 // import process from 'node:process';
 const sysprocess = require('process');
 const { exec } = require('child_process');
+const { spawn } = require('node:child_process');
 const util = require('util');
 // import { exec } from 'node:child_process';
 // import util from 'node:util';
@@ -9,32 +10,59 @@ const util = require('util');
 const execPromise = util.promisify(exec);
 const baseCmd = 'npm version';
 
+async function buildLibrary() {
+  console.info('Build Library');
+  const { stdout, stderr } = await execPromise('nx build fuego-ui');
+  printOutput({ stdout, stderr });
+  console.info('Library Built');
+}
+
+async function publishLibrary() {
+  const { stdout, stderr } = await execPromise('npm publish --dry-run');
+  printOutput({ stdout, stderr });
+  console.info('Library Published');
+}
+
+function getVersionCommand(type) {
+  switch (type) {
+    case 'major':
+      return baseCmd + ' major';
+    case 'minor':
+      return baseCmd + ' minor';
+    case 'patch':
+      return baseCmd + ' patch';
+    default:
+      throw new Error('No valid Revision Type provided');
+  }
+}
+
+function printOutput({ stdout = null, stderr = null }) {
+  if (stdout) {
+    console.log(`${stdout}`);
+  }
+  if (stderr) {
+    console.log(`${stderr}`);
+  }
+}
+
 async function buildToPublish() {
-  let versionCmd = '';
   try {
     if (!process.env.npm_config_rev) {
       throw new Error('No Revision Type provided');
     }
-    switch (process.env.npm_config_rev) {
-      case 'major':
-        versionCmd = baseCmd + ' major';
-        break;
-      case 'minor':
-        versionCmd = baseCmd + ' minor';
-        break;
-      case 'patch':
-        versionCmd = baseCmd + ' patch';
-        break;
-      default:
-        throw new Error('No valid Revision Type provided');
-    }
+    const versionCmd = getVersionCommand(process.env.npm_config_rev);
 
-    // wait for version command to complete
+    // Npm Version
+    process.chdir('./packages/fuego-ui');
     await execPromise(versionCmd);
+
+    // Library Build
     process.chdir('./../../');
-    await execPromise('nx build fuego-ui');
+    await buildLibrary();
+
+    // Publish Library
     process.chdir('./dist/packages/fuego-ui');
-    console.log(process.cwd());
+    await publishLibrary();
   } catch (error) {
     console.log(error);
   }
