faece6161438a7d9d0730fb4abf803ae53f0275a
Sammy Mohamed <sammymohamed1223@gmail.com>
2023-02-04 00:37:08 -0500

    Modal Functional

--- packages/react/src/modal/Modal.stories.tsx
+++ packages/react/src/modal/Modal.stories.tsx
@@ -5,84 +5,68 @@ import Modal from '.';
 export default {
   title: 'Components/Modal',
   component: Modal,
-  argTypes: {
-    // backgroundColor: { control: "color" },
-  },
 } as ComponentMeta<typeof Modal>;
 
-const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
+const Template: ComponentStory<typeof Modal> = (args) => {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <button className="btn btn-primary" onClick={() => setShow(!show)}>
+        Open Modal
+      </button>
+      <Modal isShowing={show} hide={() => setShow(!show)} {...args} />
+    </>
+  );
+};
 
 export const ModalCmp = Template.bind({});
 ModalCmp.args = {
-  isShowing: true,
   children: (
     <>
-      <h1>WOOW</h1>
-      <p>dmksdmlksa</p>
-      <button>COol</button>
+      <h3 className="font-bold text-lg">
+        Congratulations random Internet user!
+      </h3>
+      <p className="py-4">
+        You've been selected for a chance to get one year of subscription to use
+        Wikipedia for free!
+      </p>
+      <div className="modal-action">
+        <button className="btn">Yay!</button>
+      </div>
     </>
   ),
 };
 
 export const FullscreenModalCmp = Template.bind({});
 FullscreenModalCmp.args = {
-  isShowing: true,
   fullscreen: true,
   children: (
     <>
-      <h1>Fuego UI Modal</h1>
-      <p>Modal Text</p>
-      <button>Modal Button</button>
+      <h3 className="font-bold text-lg">
+        Congratulations random Internet user!
+      </h3>
+      <p className="py-4">
+        You've been selected for a chance to get one year of subscription to use
+        Wikipedia for free!
+      </p>
+      <div className="modal-action">
+        <button className="btn">Yay!</button>
+      </div>
     </>
   ),
 };
 
-export const OffsetModal = Template.bind({});
-OffsetModal.args = {
-  isShowing: true,
-  fullscreen: true,
-  offset: '20px',
-  className: 'bottom-0 !top-auto',
-  children: (
-    <>
-      <h1>Fuego UI Modal</h1>
-      <p>Modal Text</p>
-      <button>Modal Button</button>
-    </>
-  ),
-};
-
-export const NoBackdropModal = Template.bind({});
-NoBackdropModal.args = {
-  isShowing: true,
-  backdrop: false,
-  children: (
-    <>
-      <h1>Fuego UI Modal</h1>
-      <p>Modal Text</p>
-      <button>Modal Button</button>
-    </>
-  ),
-};
-
-const ExTemplate: ComponentStory<any> = (args) => {
-  const [show, setShow] = useState(false);
-  return (
-    <>
-      <button onClick={() => setShow(!show)}>Show Modal</button>
-      <Modal isShowing={show} hide={() => setShow(!show)} {...args} />
-    </>
-  );
-};
-
-export const Animation = ExTemplate.bind({});
-Animation.args = {
-  fullscreen: true,
-  children: (
-    <>
-      <h1>Fuego UI Modal</h1>
-      <p>Modal Text</p>
-      <button>Modal Button</button>
-    </>
-  ),
-};
+// export const OffsetModal = Template.bind({});
+// OffsetModal.args = {
+//   isShowing: true,
+//   fullscreen: true,
+//   offset: '20px',
+//   className: 'bottom-0 !top-auto',
+//   children: (
+//     <>
+//       <h1>Fuego UI Modal</h1>
+//       <p>Modal Text</p>
+//       <button>Modal Button</button>
+//     </>
+//   ),
+// };
