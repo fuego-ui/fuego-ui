7bfe43d46593fb18ab98c7723149805b0a163666
Sammy Mohamed <sammymohamed1223@gmail.com>
2023-05-16 20:35:45 -0400

    Initial Standalone accordion

--- packages/angular/src/lib/accordion/accordion.module.ts
+++ packages/angular/src/lib/accordion/accordion.module.ts
@@ -1,23 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AccordionComponent } from './accordion.component';
-import { AccordionGroupDirective } from './accordion-group.directive';
-import { AccordionHeaderDirective } from './accordion-header.directive';
-import { AccordionContentDirective } from './accordion-content.directive';
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { AccordionComponent } from './accordion.component';
+// import { AccordionGroupDirective } from './accordion-group.directive';
+// import { AccordionHeaderDirective } from './accordion-header.directive';
+// import { AccordionContentDirective } from './accordion-content.directive';
 
-@NgModule({
-  declarations: [
-    AccordionComponent,
-    AccordionGroupDirective,
-    AccordionHeaderDirective,
-    AccordionContentDirective,
-  ],
-  imports: [CommonModule],
-  exports: [
-    AccordionComponent,
-    AccordionGroupDirective,
-    AccordionHeaderDirective,
-    AccordionContentDirective,
-  ],
-})
-export class AccordionModule {}
+// @NgModule({
+//   declarations: [
+//     AccordionComponent,
+//     AccordionGroupDirective,
+//     AccordionHeaderDirective,
+//     AccordionContentDirective,
+//   ],
+//   imports: [CommonModule],
+//   exports: [
+//     AccordionComponent,
+//     AccordionGroupDirective,
+//     AccordionHeaderDirective,
+//     AccordionContentDirective,
+//   ],
+// })
+// export class AccordionModule {}
