e7f413fcdd5e5ad2cc0b4a62d98eacf3312a2fdb
Sammy Mohamed <sammymohamed1223@gmail.com>
2023-05-17 20:53:02 -0400

    Angular Lib Fix and Storybook Config Fix: - Angular upgrade to 16

--- packages/angular/src/lib/accordion/public_api.ts
+++ packages/angular/src/lib/accordion/public_api.ts
@@ -1,6 +1,2 @@
 // export { AccordionPanelComponent } from './accordion-group.component';
-export { AccordionComponent } from './accordion.component';
-export { AccordionGroupDirective } from './accordion-group.directive';
-export { AccordionHeaderDirective } from './accordion-header.directive';
-export { AccordionContentDirective } from './accordion-content.directive';
-export { AccordionModule } from './accordion.module';
+export { AccordionItemComponent } from './accordion.component';
