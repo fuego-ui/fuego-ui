69fb1165d6a68d0b2e49fcac87d351ce6b0d23d1
Sammy Mohamed <sammymohamed1223@gmail.com>
2023-09-28 00:10:33 -0400

    Input Submission: - Minor issue with File Input

--- packages/angular/src/lib/field/field.component.stories.ts
+++ packages/angular/src/lib/field/field.component.stories.ts
@@ -1,35 +1,20 @@
-// import { CommonModule } from '@angular/common';
-// import { moduleMetadata, Story, Meta } from '@storybook/angular';
-import { FieldComponent } from './field.component';
-
-// export default {
-//   title: 'Field',
-//   component: FieldComponent,
-//   decorators: [
-//     moduleMetadata({
-//       imports: [CommonModule],
-//     }),
-//   ],
-// } as Meta<FieldComponent>;
-
-// const Template: Story<FieldComponent> = (args: FieldComponent) => ({
-//   props: args,
-//   template: `<fue-field label="Username"></fue-field>
-// `,
-// });
-
-// export const Primary = Template.bind({});
-// Primary.args = {};
-
-import { CommonModule } from '@angular/common';
-import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { FueLabelDirective } from "../label/label.directive";
+import { FieldComponent } from "./field.component";
+import { InputDirective } from "./input.directive";
+import { CommonModule } from "@angular/common";
+import { Meta, moduleMetadata, StoryObj } from "@storybook/angular";
 
 const meta: Meta<FieldComponent> = {
-  title: 'Field',
+  title: "Field",
   component: FieldComponent,
   decorators: [
     moduleMetadata({
-      imports: [CommonModule, FieldComponent],
+      imports: [
+        CommonModule,
+        FieldComponent,
+        InputDirective,
+        FueLabelDirective,
+      ],
     }),
   ],
 };
@@ -39,12 +24,35 @@ type Story = StoryObj<FieldComponent>;
 
 export const Textfield: Story = {
   render: () => ({
-    props: { label: 'Username' },
+    props: {},
+    template: `<fue-field><input fueInput placeholder="placeholder" type="text"></fue-field>`,
+  }),
+};
+
+export const TextfieldWithLabel: Story = {
+  render: () => ({
+    props: {},
+    template: `<fue-field><label fueLabel>Email</label><input fueInput type="text"></fue-field>`,
   }),
 };
 
 export const Textarea: Story = {
   render: () => ({
-    props: { label: 'Username', inputType: 'textarea' },
+    props: {},
+    template: `<fue-field><textarea fueInput placeholder="Type your message here."></textarea></fue-field>`,
+  }),
+};
+
+export const TextareaWithLabel: Story = {
+  render: () => ({
+    props: {},
+    template: `<fue-field><label fueLabel>Email</label><textarea fueInput></textarea></fue-field>`,
+  }),
+};
+
+export const File: Story = {
+  render: () => ({
+    props: {},
+    template: `<fue-field><label fueLabel>Email</label><input fueInput type="file"></fue-field>`,
   }),
 };
